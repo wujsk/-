12702c8a41337b9a27fc1da032740756c84459d9
焦少康 <2524385023@qq.com>
2025-02-18 19:15:27 +0800

    实现报表统计接口(删除错误引入的包)

--- sky-server/src/main/java/com/sky/repository/ReportRepository.java
+++ sky-server/src/main/java/com/sky/repository/ReportRepository.java
@@ -1,10 +1,7 @@
 package com.sky.repository;
 
-import com.sky.entity.Orders;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 
 @Repository
 public interface ReportRepository {
